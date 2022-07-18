3bd436e65df759e526968239372fd49d525438aa
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-18 07:27:51 +0530

    updated

--- src/pages/Customer/CustomersSignUp/index.jsx
+++ src/pages/Customer/CustomersSignUp/index.jsx
@@ -3,6 +3,9 @@ import {render} from "react-dom";
 import Grid from "@material-ui/core/Grid";
 import {withStyles} from "@mui/styles";
 import {styleSheet} from "./style";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import GDSEButton from "../../../components/common/Button";
 
 class CustomerSignUp  extends Component{
 
@@ -17,15 +20,66 @@ class CustomerSignUp  extends Component{
 
         return(
             <Grid container spacing={3} className={classes.container}>
-                <Grid item xs={12} lg={7} sm={7} md={7} xm={6} className={classes.logo_container}>
+
+                <Grid item xs={12} lg={6} sm={6} md={6} xm={6} className={classes.logo_container}>
+                    <div>
+
+
+                    </div>
+
+
+                </Grid>
+                <Grid item xs={12} lg={6} sm={6} md={6} xm={6} className={classes.form_container}>
+
+                    <Grid   item xs={7} lg={7} sm={7} md={7} xm={7}  className={classes.form_container_grid}>
+
+                        <Grid   item xs={12} lg={12} sm={12} md={12} xm={12}   className={classes.form_container_grid_grid} >
+
+                            <form className={classes.form_container_grid_grid_form} noValidate autoComplete="off" >
+                                <TextField id="outlined-basic" label="Your name" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+                                <TextField id="outlined-basic" label="address" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+                                <TextField id="outlined-basic" label="telPhone number" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+                                <TextField id="outlined-basic" label="drivenLicense number" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+                                <TextField id="outlined-basic" label="Email address" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+                                <TextField id="outlined-basic" label="password" variant="outlined"  size="small"
+                                           className={classes.form_container_grid_grid_textFiled}/>
+
+
+                                <GDSEButton
+                                    label='SIGN UP'
+                                    onClick={()=>{
+
+                                        console.log('login button click..!')
+
+                                    }}
+                                    className={classes.form_container_grid_grid_button}
+
+
+                                >
+                                </GDSEButton>
+                            </form>
+
+                        </Grid>
+
+
+
+
+
+
+                    </Grid>
+
+
+
 
                 </Grid>
 
 
-                <Grid item xs={12} lg={5} sm={5} md={5} xm={6} className={classes.form_container}>
 
-
-                </Grid>
 
 
 
--- src/pages/Customer/CustomersSignUp/style.js
+++ src/pages/Customer/CustomersSignUp/style.js
@@ -21,12 +21,71 @@ export const styleSheet ={
 
 
     },
+
+
+
+
+
     form_container:{
         height: '100vh',
-        backgroundColor:'pink'
+        backgroundColor:'pink',
 
 
-    }
+
+    },
+    form_container_grid:{
+
+        height: '70%',
+        backgroundColor: '#c98744',
+        position: 'relative',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        top:'15%',
+
+
+
+
+
+    },
+    form_container_grid_grid:{
+       // marginTop:'3%',
+        border:"2px solid black",
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        width:'100%',
+        height:'100%'
+
+
+
+    },
+    form_container_grid_grid_form:{
+        width:"90%",
+        height:'90%',
+        backgroundColor:"#70a525",
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+
+    },
+
+
+
+    form_container_grid_grid_textFiled:{
+        marginTop:'3%!important',
+
+    },
+    form_container_grid_grid_button:{
+        marginTop:'3%!important',
+        width: '70%',
+        backgroundColor:"#eac249"
+
+    },
+
+
+
 
 
 
