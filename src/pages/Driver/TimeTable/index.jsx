f9c2174ffcc9ab7445ef8c4e359b461ee3e5f5ec
TharinduShantha98 <tharindushantha4@gmail.com>
2022-07-24 03:27:53 +0530

    created

--- src/pages/Driver/TimeTable/index.jsx
+++ src/pages/Driver/TimeTable/index.jsx
@@ -28,6 +28,13 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 import { appointments } from '../TimeTable/demo-data/month-appointments';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 
 
@@ -55,53 +62,118 @@ class DriverTime extends Component{
         const { data } = this.state;
 
 
-
-        // const currentDate = '2018-11-01';
-        // const schedulerData = [
-        //     { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-        //     { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-        //     { startDate: '2018-11-02T09:45', endDate: '2018-11-02T11:00', title: 'Meeting' },
-        // ];
-
         return(
 
             <div className={classes.constraint}>
 
                 <div className={classes.constraint_div1}>
 
+                    <div className={classes.constraint_div1_div1}>
+
+
+
+                    </div>
+
+
+
+
+
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Brunch this weekend?"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Ali Connors
+                                        </Typography>
+                                        {" — I'll be in your neighborhood doing errands this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Summer BBQ"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            to Scott, Alex, Jennifer
+                                        </Typography>
+                                        {" — Wish I could come, but I'm out of town this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Oui Oui"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Sandra Adams
+                                        </Typography>
+                                        {' — Do you have Paris recommendations? Have you ever…'}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                    </List>
+
+
+
+
+
+                </div>
+                <div className={classes.constraint_div2}>
+                    <Paper className={classes.constraint_div2_paper}>
+                        <Scheduler
+                            data={data}
+                        >
+                            <ViewState
+                                defaultCurrentDate="2018-07-27"
+                            />
+                            <MonthView />
+                            <Toolbar />
+                            <DateNavigator />
+                            <TodayButton />
+                            <Appointments />
+                        </Scheduler>
+                    </Paper>
+
 
                 </div>
 
 
-                <Paper>
-                    <Scheduler
-                        data={data}
-                    >
-                        <ViewState
-                            defaultCurrentDate="2018-07-27"
-                        />
-                        <MonthView />
-                        <Toolbar />
-                        <DateNavigator />
-                        <TodayButton />
-                        <Appointments />
-                    </Scheduler>
-                </Paper>
 
-               {/* <Paper>
-                    <Scheduler
-                        data={schedulerData}
-                    >
-                        <ViewState
-                            currentDate={currentDate}
-                        />
-                        <DayView
-                            startDayHour={9}
-                            endDayHour={14}
-                        />
-                        <Appointments />
-                    </Scheduler>
-                </Paper>*/}
+
+
 
             </div>
 
--- src/pages/Driver/TimeTable/style.js
+++ src/pages/Driver/TimeTable/style.js
@@ -1,14 +1,51 @@
 export const styleSheet = {
     constraint:{
-        width:'100vw',
+       // width:'100vw',
         height:'100vh',
         backgroundColor:'#722b2b',
-        display:'flex'
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
 
 
     },
 
-    d
+    constraint_div1:{
+        width:'50%',
+        height: '100%',
+        backgroundColor:'#59722b',
+        display: 'flex',
+
+
+
+    },
+    constraint_div1_div1:{
+
+
+
+
+
+    },
+
+
+
+
+
+
+    constraint_div2_paper:{
+        width:"100%",
+    },
+
+    constraint_div2:{
+        width:'50%',
+        height: '100%',
+        backgroundColor:'#2b3d72',
+        display: 'flex',
+
+
+
+    }
 
 
 
